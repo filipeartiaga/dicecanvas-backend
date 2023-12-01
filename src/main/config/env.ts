7a643441004465f8e96298540671a203fd865b03
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-01 17:30:43 -0300

    feat: add mailtrap dev credentials

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,9 +1,9 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/dicecanvas',
   port: process.env.PORT || 5050,
-  mailHost: process.env.MAIL_HOST || '127.0.0.1',
-  mailPort: process.env.MAIL_PORT || 1025,
-  mailUser: process.env.MAIL_USER || '',
-  mailPass: process.env.MAIL_PASS || '',
+  mailHost: process.env.MAIL_HOST || 'sandbox.smtp.mailtrap.io',
+  mailPort: process.env.MAIL_PORT || 2525,
+  mailUser: process.env.MAIL_USER || 'df4b4f084212cb',
+  mailPass: process.env.MAIL_PASS || '1c2041a7829325',
   jwtSecret: process.env.JWT_SECRET || 'tj67O==5H'
 }
