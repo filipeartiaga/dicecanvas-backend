273209936b61e7e1090fafd6667329396fb0796f
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-17 23:03:10 -0300

    feat: create data usecase for get all logs

---
+++ src/data/usecases/log/db-get-all-logs.ts
@@ -0,0 +1,16 @@
+import { GetAllLogsRepository } from '../../protocols/log/get-all-logs-repository'
+import { LogModel } from '../../../domain/models/log/log'
+import { GetAllLogs } from '../../../domain/usescases/log/get-all-logs'
+
+export class DbGetAllLog implements GetAllLogs {
+  private readonly getAllLogRepository: GetAllLogsRepository
+
+  constructor (getAllLogRepository: GetAllLogsRepository) {
+    this.getAllLogRepository = getAllLogRepository
+  }
+
+  async getAll (): Promise<LogModel[]> {
+    const log = await this.getAllLogRepository.getAll()
+    return log
+  }
+}
