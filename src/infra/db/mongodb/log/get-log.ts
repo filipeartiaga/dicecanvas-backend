b2a09675ce962382fd30c61eeafe6afe30a47f27
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-20 02:16:07 -0300

    feat: create get longo mongodb repository

--- src/infra/db/mongodb/log/get-log.ts
+++ src/infra/db/mongodb/log/get-log.ts
@@ -3,7 +3,7 @@ import { ObjectId } from 'mongodb'
 import { MongoHelper } from '../helpers/mongo-helper'
 
 export class GetLogMongoRepository implements GetLogMongoRepository {
-  async getById (_id: string): Promise<LogModel[]> {
+  async getById (_id: string): Promise<LogModel> {
     const logCollection = MongoHelper.getCollection('logs')
     const userIdObjectId = new ObjectId(_id)
     const log = await logCollection.findOne({ _id: userIdObjectId })
