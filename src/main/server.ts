aa0a6ca61ab96960138182ba9941d20b170b0953
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 19:36:21 -0300

    feat: ensure SocketIoServer replies message to all connected clients

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,6 +4,15 @@ import env from './config/env'
 MongoHelper.connect(env.mongoUrl)
   .then(async () => {
     const app = (await import('./config/app')).default
-    app.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) })
+    const server = (await import('./config/httpServer')).default(app)
+    const io = (await import('./config/io')).default(server)
+
+    io.on('connection', (socket) => {
+      socket.on('message', (data) => {
+        io.emit('message', data)
+      })
+    })
+
+    server.listen(env.port, () => { console.log(`Server is running at http://localhost:${env.port}`) })
   })
   .catch(console.error)
