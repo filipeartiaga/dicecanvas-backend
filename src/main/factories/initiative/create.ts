9b530b8bd9c705e0f0aac55c3b977690db963464
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 15:41:45 -0300

    feat: create makeCreateInitiativeController factory

---
+++ src/main/factories/initiative/create.ts
@@ -0,0 +1,19 @@
+import { AuthenticatedValidatorAdapter, UserDecoderAdapter } from '../../../utils/user'
+import { GetUserMongoRepository } from '../../../infra/db/mongodb/user-repository/get-user'
+import { DbGetUser } from '../../../data/usecases/user/db-get-user'
+import env from '../../config/env'
+import { CreateInitiativeController } from 'src/presentation/controllers/initiative/create'
+import { AddInitiativeMongoRepository } from 'src/infra/db/mongodb/initiative/add-initiative'
+import { DbAddInitiative } from 'src/data/usecases/initiative/db-add-initiative'
+
+export const makeCreateInitiativeController = (): CreateInitiativeController => {
+  const getUserMongoRepository = new GetUserMongoRepository()
+  const dbGetUser = new DbGetUser(getUserMongoRepository)
+  const secret = env.jwtSecret
+  const userDecoderAdapter = new UserDecoderAdapter(secret)
+  const authenticatedValidatorAdapter = new AuthenticatedValidatorAdapter(dbGetUser, userDecoderAdapter)
+  const addInitiativeMongoRepository = new AddInitiativeMongoRepository()
+  const dbAddInitiative = new DbAddInitiative(addInitiativeMongoRepository)
+  const controller = new CreateInitiativeController(authenticatedValidatorAdapter, dbAddInitiative)
+  return controller
+}
