0acccc80dbe75dc02accce7f485d4112f22e7280
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 15:50:40 -0300

    feat: create makeClearInitiativeController factory

---
+++ src/main/factories/initiative/clear.ts
@@ -0,0 +1,19 @@
+import { AuthenticatedValidatorAdapter, UserDecoderAdapter } from '../../../utils/user'
+import { GetUserMongoRepository } from '../../../infra/db/mongodb/user-repository/get-user'
+import { DbGetUser } from '../../../data/usecases/user/db-get-user'
+import env from '../../config/env'
+import { ClearInitiativeController } from 'src/presentation/controllers/initiative/clear'
+import { ClearInitiativeMongoRepository } from 'src/infra/db/mongodb/initiative/clear-initiative'
+import { DbClearInitiative } from 'src/data/usecases/initiative/db-clear-initiative'
+
+export const makeClearInitiativeController = (): ClearInitiativeController => {
+  const getUserMongoRepository = new GetUserMongoRepository()
+  const dbGetUser = new DbGetUser(getUserMongoRepository)
+  const secret = env.jwtSecret
+  const userDecoderAdapter = new UserDecoderAdapter(secret)
+  const authenticatedValidatorAdapter = new AuthenticatedValidatorAdapter(dbGetUser, userDecoderAdapter)
+  const clearInitiativeMongoRepository = new ClearInitiativeMongoRepository()
+  const dbClearInitiative = new DbClearInitiative(clearInitiativeMongoRepository)
+  const controller = new ClearInitiativeController(authenticatedValidatorAdapter, dbClearInitiative)
+  return controller
+}
