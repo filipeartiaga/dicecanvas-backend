e9727e83738b1d5681f5635a6d90f08d09946d1c
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 15:53:59 -0300

    feat: create get all initiative route

---
+++ src/main/routes/initiative/get-all-initiatives-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express'
+import { adaptRoute } from '../../adapters/express-route-adapter'
+import { makeGetAllInitiativeController } from 'src/main/factories/initiative/get-all'
+
+export default (router: Router): void => {
+  router.get('/initiatives/', adaptRoute(makeGetAllInitiativeController()))
+}
