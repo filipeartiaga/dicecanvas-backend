f603a7a083d2e78ba4ee2181426aa676a192cd48
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 15:47:17 -0300

    fix: change unnecessary injection dependencies

--- src/presentation/controllers/initiative/get-all.ts
+++ src/presentation/controllers/initiative/get-all.ts
@@ -4,17 +4,14 @@ import { ok, serverError, unauthorized } from '../../helpers/http-helpers'
 import { UnauthorizedError } from '../../errors'
 import { AuthenticatedValidator } from '../../protocols/user'
 import { AllInitiativeGetter } from '../../protocols/initiative'
-import { CharacterSheetGetter } from '../../protocols/character-sheet'
 
 export class GetAllInitiativeController implements Controller {
   private readonly authenticatedValidator: AuthenticatedValidator
   private readonly allInitiativeGetter: AllInitiativeGetter
-  private readonly characterSheetGetter: CharacterSheetGetter
 
-  constructor (authenticatedValidator: AuthenticatedValidator, allInitiativeGetter: AllInitiativeGetter, characterSheetGetter: CharacterSheetGetter) {
+  constructor (authenticatedValidator: AuthenticatedValidator, allInitiativeGetter: AllInitiativeGetter,) {
     this.authenticatedValidator = authenticatedValidator
     this.allInitiativeGetter = allInitiativeGetter
-    this.characterSheetGetter = characterSheetGetter
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
