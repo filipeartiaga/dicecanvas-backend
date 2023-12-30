fe0ff5b7178f9da6781e195fbddbed58f4de05a5
filipeartiaga <filipeaartiaga@gmail.com>
2023-12-30 15:46:09 -0300

    fix: change name of implementation of class

--- src/infra/db/mongodb/initiative/clear-initiative.ts
+++ src/infra/db/mongodb/initiative/clear-initiative.ts
@@ -1,6 +1,6 @@
 import { MongoHelper } from '../helpers/mongo-helper'
 
-export class DeleteInitiativeMongoRepository implements DeleteInitiativeMongoRepository {
+export class ClearInitiativeMongoRepository implements ClearInitiativeMongoRepository {
   async clear (): Promise<any> {
     const initiativesCollection = MongoHelper.getCollection('initiatives')
     const response = await initiativesCollection.delete()
