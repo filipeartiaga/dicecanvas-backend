75cd18268e46dd99308834f2995feb3c586f3a0f
filipeartiaga <filipeaartiaga@gmail.com>
2024-01-06 00:28:05 -0300

    feat: allow websocket origin from anywhere

--- src/main/config/io.ts
+++ src/main/config/io.ts
@@ -4,10 +4,7 @@ import { Server as SocketIOServer } from 'socket.io'
 const makeIo = (server: Server): SocketIOServer => {
   const io = require('socket.io')(server, {
     cors: {
-      origin: 'https://dicecanvas.com',
-      methods: ['GET', 'POST'],
-      allowedHeaders: ['my-custom-header'],
-      credentials: true
+      origin: '*'
     }
   })
 
