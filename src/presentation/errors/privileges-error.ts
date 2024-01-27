5ee7e738956722133c39c459a347c252a9f9fb28
filipeartiaga <filipeaartiaga@gmail.com>
2024-01-26 23:51:35 -0300

    refactor: remove unused error

--- src/presentation/errors/privileges-error.ts
+++
@@ -1,6 +0,0 @@
-export class PrivilegesError extends Error {
-  constructor () {
-    super('User not allowed to perform this action')
-    this.name = 'User not allowed to perform this action'
-  }
-}
